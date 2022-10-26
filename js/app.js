3529b67e448cbe8435cab2d694be0dd2ea7893a7
Carlos Alba <hola@carlosalba.net>
2022-10-26 16:01:43 +0300

    HTML y CSS listos

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const formulario = document.querySelector(".formulario"),
+  email = document.getElementById("email").value,
+  asunto = document.getElementById("asunto").value,
+  mensaje = document.getElementById("mensaje").value,
+  btnReset = document.querySelector(".reset"),
+  btnEnviar = document.querySelector(".enviar");
+
+console.log(formulario, email, asunto, mensaje, btnReset, btnEnviar);
